8211be3b1657b87ff2ffe2d66677a47305493c62
Farshid@wifi <User@DEV-VM>
2023-09-28 20:57:46 +0200

    Beispiel vom 26.09.2023- ester Abend Einstieg

---
+++ src/20230926/HelloWorldApplication/HelloWorldApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello liebe Leute....");
+            Console.WriteLine("C# ist toll...");
+
+            Console.Beep();
+
+            Console.ReadLine();
+
+        }
+    }
+}
