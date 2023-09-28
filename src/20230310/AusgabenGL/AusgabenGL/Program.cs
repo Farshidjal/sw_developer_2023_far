4055d013fee384d87d2269e9bd8fa0d0bc047901
Farshid@wifi <User@DEV-VM>
2023-09-28 23:42:35 +0200

    Grundlagen

---
+++ src/20230310/AusgabenGL/AusgabenGL/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AusgabenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Juhuu...");
+            Console.WriteLine();
+            Console.Write("Juhuu1...");
+
+            Console.Beep(1000, 50);
+            Console.WriteLine("Dies \nist ein test\n \t \a... Hier gehts. ");
+            /*
+             *      Name:    [ Jalali]
+             *      Alter:   [54] Jahre
+             *      
+             *      Wohnort: [Feldkirch]
+             *      
+             *C# ist interesante program sprache!
+             *
+             */
+            Console.WriteLine("\tName: \t Farshid");
+            Console.WriteLine("Alter: \t\a54");
+            Console.WriteLine("Wohnort: \tFeldkirch");
+            Console.WriteLine("C# ist interesante Program sprache!");
+            
+            
+        }
+        
+    }
+}
