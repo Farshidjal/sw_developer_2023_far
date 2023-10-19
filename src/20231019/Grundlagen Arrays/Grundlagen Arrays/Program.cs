60543d447650e4165704ce391e447cd373afec8c
Farshid <farshid47jal@gmail.com>
2023-10-19 19:30:57 +0200

    Grundlagen Arrays # ; closes#

---
+++ src/20231019/Grundlagen Arrays/Grundlagen Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grundlagen_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl = 4;
+            const int MAX_ITEM_COUNT = 40;
+
+            int[] zahlen = new int[MAX_ITEM_COUNT];
+            //double[]
+            //Teilnehmer[] mewinTeilnehmerListe;
+
+            zahlen = new int[MAX_ITEM_COUNT];
+
+            zahlen[0] = 45;
+            zahlen[2] = 50;
+
+            Console.WriteLine($"2. wert: {zahlen[1]}");
+
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                zahlen[i] = 0;
+            }
+        }
+    }
+}
